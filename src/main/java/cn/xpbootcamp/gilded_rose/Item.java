e97d73daeb619ff3a49f11e069be872b989512fa
Yuan Shenjian <sjyuan@thoughtworks.com>
2020-05-21 08:28:17 +0800

    I.4 Polish three phases with Guard Clauses and condition statement reversion
    Signed-off-by: Yuan Shenjian <sjyuan@thoughtworks.com>

--- src/main/java/cn/xpbootcamp/gilded_rose/Item.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/Item.java
@@ -34,49 +34,51 @@ public class Item {
     }
 
     private void updateQuality() {
-        if (!isAgedBrie()
-                && !isBackstagePass()) {
-            if (quality > 0) {
-                if (!isSulfuras()) {
-                    quality = quality - 1;
-                }
-            }
-        } else {
+        if (isAgedBrie()) {
             if (quality < 50) {
                 quality = quality + 1;
-
-                if (isBackstagePass()) {
-                    if (sellIn < 11) {
-                        increaseQuality();
-                    }
-
-                    if (sellIn < 6) {
-                        increaseQuality();
-                    }
-                }
             }
+            return;
+        }
+        if (isBackstagePass()) {
+            increaseQuality();
+            if (sellIn < 11) {
+                increaseQuality();
+            }
+            if (sellIn < 6) {
+                increaseQuality();
+            }
+            return;
+        }
+        if (isSulfuras()) {
+            return;
+        }
+        if (quality > 0) {
+            quality = quality - 1;
         }
     }
 
     private void updateSellIn() {
-        if (!isSulfuras()) {
-            sellIn = sellIn - 1;
+        if (isSulfuras()) {
+            return;
         }
+        sellIn = sellIn - 1;
     }
 
     private void updateQualityAfterExpired() {
-        if (!isAgedBrie()) {
-            if (!isBackstagePass()) {
-                if (quality > 0) {
-                    if (!isSulfuras()) {
-                        quality = quality - 1;
-                    }
-                }
-            } else {
-                quality = 0;
-            }
-        } else {
+        if (isAgedBrie()) {
             increaseQuality();
+            return;
+        }
+        if (isBackstagePass()) {
+            quality = 0;
+            return;
+        }
+        if (isSulfuras()) {
+            return;
+        }
+        if (quality > 0) {
+            quality = quality - 1;
         }
     }
 
